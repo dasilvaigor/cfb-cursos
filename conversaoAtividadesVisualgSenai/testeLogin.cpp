b4180063e0dcaea61be4f650014b114d7f576683
dasilvaigor <128702312+dasilvaigor@users.noreply.github.com>
2023-05-02 09:45:33 -0300

    Add files via upload

--- conversaoAtividadesVisualgSenai/testeLogin.cpp
+++ conversaoAtividadesVisualgSenai/testeLogin.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 int main (){
 	
+	//declaração das variavéis
 	string usuarioCadastrado = "igorsilva", senhaCadastrada = "123456";
 	string usuario, senha;
 	
+	//solicitação dos dados ao usuário
 	cout << "Informe o usuario: ";
 	cin >> usuario;
 	cout << "Informe a senha: ";
 	cin >> senha;
 	
-	
+	//comando para limpar a tela
 	system ("cls");
 	
+	//verificação das condições e exibição da informação na tela
 	if (usuario == usuarioCadastrado & senha == senhaCadastrada) {
 		cout << "Bem vindo Igor!\n";
 	} else {
