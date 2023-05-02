e7839a190bab2bf615a0baa3598ffbb15a23a9e3
dasilvaigor <128702312+dasilvaigor@users.noreply.github.com>
2023-05-02 09:42:43 -0300

    Add files via upload

--- conversaoAtividadesVisualgSenai/voto.cpp
+++ conversaoAtividadesVisualgSenai/voto.cpp
@@ -3,16 +3,20 @@
 using namespace std;
 int main (){
 	
+	//declaração de variavéis
 	string nome;
 	int idade=0;
 	
+	//solicitação de dados ao usuário
 	cout << "Informe o seu nome: ";
 	cin >> nome;
 	cout << "Informe sua idade: ";
 	cin >> idade;
 	
+	//comando para limpar a tela
 	system("cls");
 	
+	//verificação das condições para exibição da informação na tela
 	if (idade >= 18 & idade <= 65) {
 		cout << "Seu voto e obrigatorio!\n\n";
 	} else {
